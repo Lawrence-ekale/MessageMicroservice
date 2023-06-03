b8c66fde061575e1540aa07ad24d239727fce356
Lawrence-ekale <lawrenceekale2@gmail.com>
2023-06-03 14:01:07 +0300

    Proto generate files

---
+++ messageGRPC/src/main/java/com/lawrenceekale/messageGRPC/MessageGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.lawrenceekale.messageGRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageGrpcApplication.class, args);
+	}
+
+}
---
+++ messageGRPC/src/test/java/com/lawrenceekale/messageGRPC/MessageGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawrenceekale.messageGRPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
